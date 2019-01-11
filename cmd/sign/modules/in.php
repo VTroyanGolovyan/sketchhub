d5e4de4aec0df2258123f421847e574f318ce971
VladislavHacker <vladislavhacker111@gmail.com>
2019-01-11 23:36:28 +0200

    bugfix

--- cmd/sign/modules/in.php
+++ cmd/sign/modules/in.php
@@ -2,7 +2,7 @@
   if (isset($_POST['email']) && isset($_POST['password'])){
     $email = htmlspecialchars($_POST['email']);
     $password = md5(md5(htmlspecialchars($_POST['password'])));
-    $query = 'SELECT `id`,`access`,`password` FROM `users` WHERE email = "'.$email .'" LIMIT 1';
+    $query = 'SELECT `id`,`access`,`password` FROM `users` WHERE `access` != "0" AND `email` = "'.$email .'" LIMIT 1';
     $rez = $mysqli->query($query);
     if ($rez->num_rows != 0){
       $row = $rez->fetch_assoc();
